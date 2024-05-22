35bf8ea5e63e521f88424363248f849a44c858f9
PTho <phamvantho042002@gmail.com>
2024-05-22 23:41:26 +0700

    commit lan 4

--- app/src/main/java/com/example/myapplication/Activity/DetailActivity.java
+++ app/src/main/java/com/example/myapplication/Activity/DetailActivity.java
@@ -1,16 +1,57 @@
 package com.example.myapplication.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
 
+import com.bumptech.glide.Glide;
+import com.example.myapplication.Domain.PopularDomain;
+import com.example.myapplication.Helper.ManagmentCart;
 import com.example.myapplication.R;
+import com.example.myapplication.databinding.ActivityDetailBinding;
 
 public class DetailActivity extends AppCompatActivity {
-
+    private ActivityDetailBinding binding;
+    private PopularDomain object;
+    private int numberOrder=1;
+    private ManagmentCart managmentCart;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_detail);
+        binding=ActivityDetailBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        getBundles();
+        managmentCart=new ManagmentCart(this);
+        statusBarColor();
+    }
+    private void statusBarColor() {
+        Window window=DetailActivity.this.getWindow();
+        window.setStatusBarColor(ContextCompat.getColor(DetailActivity.this, R.color.white));
+    }
+
+    private void getBundles() {
+        object= (PopularDomain) getIntent().getSerializableExtra("object");
+
+        int drawableResourceId=this.getResources().getIdentifier(object.getPicUrl(),"drawable",this.getPackageName());
+        Glide.with(this)
+                .load(drawableResourceId)
+                .into(binding.itemPic);
+
+        binding.titleTxt.setText(object.getTitle());
+        binding.priceTxt.setText("$"+object.getPrice());
+        binding.descriptionTxt.setText(object.getDescription());
+        binding.reviewTxt.setText(object.getReview()+"");
+        binding.ratingTxt.setText(object.getScore()+"");
+
+        binding.addToCardBtn.setOnClickListener(v -> {
+            object.setNumberInCart(numberOrder);
+            managmentCart.insertFood(object);
+        });
+
+        binding.backBtn.setOnClickListener(v -> finish());
     }
 }
--- app/src/main/java/com/example/myapplication/Adapter/PopularAdapter.java
+++ app/src/main/java/com/example/myapplication/Adapter/PopularAdapter.java
@@ -11,6 +11,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.resource.bitmap.GranularRoundedCorners;
+import com.example.myapplication.Activity.DetailActivity;
 import com.example.myapplication.Domain.PopularDomain;
 import com.example.myapplication.databinding.ViewholderPupListBinding;
 
@@ -38,7 +39,7 @@ public class PopularAdapter extends RecyclerView.Adapter<PopularAdapter.Viewhold
         binding.titleTxt.setText(items.get(position).getTitle());
         binding.feeTxt.setText("$" + items.get(position).getPrice());
         binding.scoreText.setText("" + items.get(position).getScore());
-
+        binding.reviewTxt.setText("" + items.get(position).getReview());
         int drawableResourced=holder.itemView.getResources().getIdentifier(items.get(position).getPicUrl()
                 ,"drawable",holder.itemView.getContext().getPackageName());
 
@@ -47,11 +48,10 @@ public class PopularAdapter extends RecyclerView.Adapter<PopularAdapter.Viewhold
                 .transform(new GranularRoundedCorners(30,30,0,0))
                 .into(binding.pic);
 
-        holder.itemView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
+        holder.itemView.setOnClickListener(v -> {
+            Intent intent=new Intent(context, DetailActivity.class);
+            intent.putExtra("object", items.get(position));
+            context.startActivity(intent);
         });
     }
 
